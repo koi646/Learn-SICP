bcaa7b786a25480238342577e3080df4b2c67ab7
koi646 <lyh472617147@icloud.com>
2015-10-16 11:27:39 +0800

    *task1.10

---
+++ unit1/*task1.10.rkt
@@ -0,0 +1,18 @@
+#lang r5rs
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+;表示2的十次方
+(display (A 1 10))
+(newline)
+;
+(display (A 2 4))
+(newline)
+(display (A 2 3))
+(newline)
+(display (A 3 3))
+(newline)
+(display (A 3 2))
