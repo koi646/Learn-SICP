6c6b0c72e1f4d90a59e71d00021867ec4a2ecb11
koi646 <lyh472617147@icloud.com>
2015-10-18 13:56:05 +0800

    修正1.15

--- unit1/task1.16.rkt
+++ unit1/task1.16.rkt
@@ -1,10 +1,15 @@
 #lang r5rs
 (define (pow a n)
+  ;判断是否偶数
+  (define (even? x)
+    (= (remainder x 2) 0))
+  ;平方
   (define (square x)
     (* x x))
   (define (iter sum num)
-    (cond ((= num 1) (iter (* sum a) (- num 1)))
-          ((= num 0) sum)
-          (else (iter (* sum (square a)) (- num 2)))))
+    (cond ((= num 0) sum)
+          ((even? num) (square (iter sum (/ num 2))))
+          (else (* a (iter sum (- num 1))))))
   (iter 1 n))
-(display (pow 2 4))
+(display (pow 2 11))
+(newline)
