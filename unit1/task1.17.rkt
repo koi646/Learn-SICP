dd9eff3636ea53d5d58484d5e117977117030f34
koi646 <lyh472617147@icloud.com>
2015-10-18 16:09:55 +0800

    task1.17更正

--- unit1/task1.17.rkt
+++ unit1/task1.17.rkt
@@ -1,5 +1,16 @@
 #lang r5rs
-#|
-与上题类似
-略
-|#
+(define (* a b)
+  (define (double x)
+    (+ x x))
+  ;折半
+  (define (halve x)
+    (/ x 2))
+  ;是否偶数
+  (define (even? number)
+    (= (remainder number 2) 0))
+  (define (iter sum y)
+    (cond ((= y 0) sum)
+          ((even? y) (double (iter sum (halve y))))
+          (else  (+ a (iter sum (- y 1))))))
+  (iter 0 b))
+(display (* 10 5))
