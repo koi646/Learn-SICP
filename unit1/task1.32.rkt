ce7d8ab6431a39f228ba55857dc246b7a3095f2c
koi646 <lyh472617147@icloud.com>
2015-10-21 15:31:18 +0800

    task1.34

--- unit1/task1.32.rkt
+++ unit1/task1.32.rkt
@@ -6,6 +6,7 @@
 (define (accumulate combiner null-value term a next b)
   (if (> a b) null-value
       (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+;定义出sum-cube
 (define (sum term a next b)
   (define (fn a b)
     (+ a b))
