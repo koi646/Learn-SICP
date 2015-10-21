cb363be2645158a4379ae766d5b66b4e3a82e43b
koi646 <lyh472617147@icloud.com>
2015-10-21 14:26:09 +0800

    task1.32

---
+++ unit1/task1.32.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(define (cube x)
+  (* x x x))
+(define (next x)
+  (+ x 1))
+(define (accumulate combiner null-value term a next b)
+  (if (> a b) null-value
+      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+(define (sum term a next b)
+  (define (fn a b)
+    (+ a b))
+  (accumulate fn 0 term a next b))
+(define (sum-cube a b)
+  (sum cube a next b))
+(sum-cube 1 3)
