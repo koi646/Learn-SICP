c9d3b4e9b291bed4d6a822f92f741524face90d9
koi646 <lyh472617147@icloud.com>
2015-10-26 14:30:45 +0800

    task2.1

--- unit2/task2.1.rkt
+++ unit2/task2.1.rkt
@@ -2,17 +2,23 @@
 ; numer返回分子 denom返回分母
 (define numer car)
 (define denom cdr)
-;绝对值
+;工具函数   绝对值
 (define (abs x)
   (if (> x 0) x
       (- 0 x)))
+;最大公约数
+(define (gcd a b)
+  (if (= b 0) a
+      (gcd b (remainder a b))))
 (define (make-rat n d)
-  (cond ((>= (* n d) 0) (cons (abs n) (abs d)))
-        (else (cons (- 0 (abs n)) (abs d)))))
+  (let  ((GCD (gcd n d)))
+  (cond ((>= (* n d) 0) (cons (abs (/ n GCD)) (abs (/ d GCD))))
+        (else (cons (- 0 (abs (/ n GCD))) (abs (/ d GCD)))))))
 (define (print-rat x)
+
   (newline)
   (display (numer x))
   (display "/")
   (display (denom x)))
-(define value (make-rat 3 -5))
+(define value (make-rat 3 -6))
 (print-rat value)
