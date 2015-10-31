10903255f8c64a6461140290925a733e530f050b
koi646 <lyh472617147@icloud.com>
2015-10-31 15:55:42 +0800

    task2.23

---
+++ unit2/task2.23.rkt
@@ -0,0 +1,6 @@
+#lang racket
+(define (for-each iter items)
+  (cond ((null? items) )
+        (else (iter (car items))
+              (for-each iter (cdr items)))))
+(for-each (lambda (i) (newline) (display i)) (list 1 2 3))
